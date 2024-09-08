e2b4a9e54bf2fe10657b3f804631b1827097ec68
Jinwoo Jeong <thinkratomos@gmail.com>
2024-09-09 00:02:15 +0900

    initial commit

---
+++ tadaktadak/tadaktadakApp.swift
@@ -0,0 +1,17 @@
+//
+//  tadaktadakApp.swift
+//  tadaktadak
+//
+//  Created by 정진우 on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct tadaktadakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
